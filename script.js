9a609f22740b319ef3f93b6d88e6ac95a79d4ec9
framos500 <142738685+framos500@users.noreply.github.com>
2024-01-29 01:16:42 -0300

    Update script.js
    Signed-off-by: framos500 <142738685+framos500@users.noreply.github.com>

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ document.addEventListener("DOMContentLoaded", function () {
     var roboContainer = document.getElementById("robo-container");
 
     roboContainer.addEventListener("click", function () {
-        // Adicione aqui a lógica para abrir o seu chatbot
         alert("Chatbot aberto!"); // Exemplo: abrir um alerta
     });
 });
