75da391c52ffdd25dd34e6b4c442cf87305c70e5
framos500 <142738685+framos500@users.noreply.github.com>
2024-01-29 01:27:38 -0300

    Update script.js
    Signed-off-by: framos500 <142738685+framos500@users.noreply.github.com>

--- robo_project/script.js
+++ robo_project/script.js
@@ -1,7 +1,22 @@
 document.addEventListener("DOMContentLoaded", function () {
     var roboContainer = document.getElementById("robo-container");
+    var mensagemInput = document.getElementById("mensagemInput");
 
     roboContainer.addEventListener("click", function () {
-        alert("Chatbot aberto!"); // Exemplo: abrir um alerta
+        alert("Chatbot aberto!");
+        mostrarChatBox(); // Adiciona esta linha para mostrar a caixa de texto
     });
+
+    function mostrarChatBox() {
+        var chatBox = document.querySelector('.chat-box');
+        chatBox.style.display = 'flex';
+    }
+
+    window.enviarMensagem = function () {
+        var mensagem = mensagemInput.value;
+        // Faça algo com a mensagem, como exibir, enviar para um servidor, etc.
+        alert("Mensagem enviada: " + mensagem);
+        // Limpe o campo de entrada
+        mensagemInput.value = "";
+    };
 });
