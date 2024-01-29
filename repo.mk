d4fe1a7b545a56d2d5d7cf5c076aeb4efe3bc9d4
framos500 <142738685+framos500@users.noreply.github.com>
2024-01-28 23:10:26 -0300

    Create repo.mk

---
+++ repo.mk
@@ -0,0 +1,7 @@
+echo "# brain.io.github_main" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/framos500/brain.io.github_main.git
+git push -u origin main
